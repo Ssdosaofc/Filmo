e34904245886f44cc31fa565c855d826633e4490
Ssdosaofc <147481143+Ssdosaofc@users.noreply.github.com>
2023-12-18 23:26:19 +0530

    update the changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -48,16 +48,16 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //Recyclerview
-    implementation ("androidx.recyclerview:recyclerview:1.1.0-alpha02")
+    implementation ("androidx.recyclerview:recyclerview:1.4.0-alpha01")
     implementation ("androidx.cardview:cardview:1.0.0")
     // Gson
-    implementation ("com.google.code.gson:gson:2.8.5")
+    implementation ("com.google.code.gson:gson:2.9.1")
 
     // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.5.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.retrofit2:adapter-rxjava2:2.5.0")
 
     // Glide
-    implementation ("com.github.bumptech.glide:glide:4.8.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
