aa8c6d996aa4f50e5bbe5aac222e9ca433ebd15a
Ssdosaofc <147481143+Ssdosaofc@users.noreply.github.com>
2023-12-19 12:13:23 +0530

    update the changes

--- app/src/main/java/com/example/main/Login/RegisterActivity.kt
+++ app/src/main/java/com/example/main/Login/RegisterActivity.kt
@@ -13,11 +13,14 @@ import com.example.main.MainActivity
 import com.example.main.R
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var db: FirebaseDatabase
+    private lateinit var root: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +31,8 @@ class RegisterActivity : AppCompatActivity() {
         val editTextVerifyPassword = findViewById<TextInputEditText>(R.id.verifypassword)
         val buttonReg = findViewById<Button>(R.id.reg)
         val textView = findViewById<TextView>(R.id.loginNow)
+        db = FirebaseDatabase.getInstance()
+        root = db.getReference().child("Users")
 
         textView.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -53,6 +58,12 @@ class RegisterActivity : AppCompatActivity() {
             } else {
                 auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
+                        val userMap = HashMap<String, String>()
+                        userMap["email"] = email
+                        userMap["password"] = password
+
+                        root.setValue(userMap)
+
                         Toast.makeText(this, "Account created", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
