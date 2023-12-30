1ee815777e5d2719d6354537a37da4b03d794a18
Ssdosaofc <147481143+Ssdosaofc@users.noreply.github.com>
2023-12-30 12:01:21 +0530

    update the changes

---
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.main.Login.LoginActivity
+import com.example.main.R
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
